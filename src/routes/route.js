265be31e8986e2ea20b89a0dc97ed68024c24a34
Prasurjya Pran Borah <96339995+ppborah@users.noreply.github.com>
2022-05-27 00:56:25 +0530

    updated route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,11 +8,26 @@ const {
   updateUser,
 } = require("../controllers/userController");
 
+const {
+  createProduct,
+  getProduct,
+  getProductById,
+  updateProduct,
+  deleteProductById,
+} = require("../controllers/productController");
+
 // user APIs
 router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/user/:userId/profile", authentication, getProfile);
 router.put("/user/:userId/profile", authentication, updateUser);
+
+// product APIs
+router.post("/products", createProduct);
+router.get("/products", getProduct);
+router.get("/products/:productId", getProductById);
+router.put("/products/:productId", updateProduct);
+router.delete("/products/:productId", deleteProductById);
 
 router.all("/*", function (req, res) {
   res
